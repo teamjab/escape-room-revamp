806f4e930bf517754de81369618eebac007d7176
BrendonLH <Brendonhampton1988@gmail.com>
2021-10-15 14:20:11 -0700

    initial commit

--- escape-room/_test_/api.test.js
+++ escape-room/_test_/api.test.js
@@ -1,6 +1,13 @@
 const sum = require("./apitest.js");
+const getRequest = require("./apitest.js");
 
+const url = "https://catfact.ninja/breeds";
 // basic starter test to link up test suite
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+describe("api tests", () => {
+  test("adds 1 + 2 to equal 3", () => {
+    expect(sum(1, 2).toBe(3));
+  });
+  test("checks if returns API response", () => {
+    expect(getRequest(url)).toBe(undefined);
+  });
 });
