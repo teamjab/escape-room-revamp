aa4e0bab7ec2399b0140adfb17773aef3c64d6f0
Jin Kim <jinwoov@gmail.com>
2021-10-15 16:29:51 -0700

    Testing-Pipeline

--- api/main.go
+++ api/main.go
@@ -1,5 +1,7 @@
 package main
 
+// Riddles obtained from https://parade.com/947956/parade/riddles/
+
 import (
 	"encoding/json"
 	"net/http"
@@ -22,7 +24,66 @@ func getRiddle(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	var riddles = []Riddles.Riddle{
-		Riddles.Riddle{Username: "Brendon", Question: "Make a fist", Answer: "And punch yourself"},
+		{
+			Username: "Brendon",
+			Question: "Make a fist",
+			Answer: []Riddles.Answers{
+				{
+					AnswerOne:   "Punch yourself",
+					AnswerTwo:   "Punch Brendon",
+					AnswerThree: "Punch Brendon",
+					AnswerFour:  "Punch Brendon",
+				},
+			},
+		},
+		{
+			Username: "TeamJab",
+			Question: "What has to be broken before you can use it?",
+			Answer: []Riddles.Answers{
+				{
+					AnswerOne:   "An egg",
+					AnswerTwo:   "A life",
+					AnswerThree: "Brendon's face",
+					AnswerFour:  "Jin's face",
+				},
+			},
+		},
+		{
+			Username: "TeamJab",
+			Question: "What month of the year has 28 days?",
+			Answer: []Riddles.Answers{
+				{
+					AnswerOne:   "Febuary",
+					AnswerTwo:   "March",
+					AnswerThree: "All of them",
+					AnswerFour:  "DecemberMarch",
+				},
+			},
+		},
+		{
+			Username: "TeamJab",
+			Question: "What has a head and a tail but no body?",
+			Answer: []Riddles.Answers{
+				{
+					AnswerOne:   "Brendon",
+					AnswerTwo:   "Jin",
+					AnswerThree: "Life",
+					AnswerFour:  "Coin",
+				},
+			},
+		},
+		{
+			Username: "TeamJab",
+			Question: "What can travel all around the world without leaving its corner?",
+			Answer: []Riddles.Answers{
+				{
+					AnswerOne:   "My car",
+					AnswerTwo:   "Air plane",
+					AnswerThree: "Yo mama",
+					AnswerFour:  "A stamp",
+				},
+			},
+		},
 	}
 	json.NewEncoder(w).Encode(riddles)
 }
--- api/models/riddles.go
+++ api/models/riddles.go
@@ -1,7 +1,14 @@
 package Riddles
 
+type Answers struct {
+	AnswerOne   string
+	AnswerTwo   string
+	AnswerThree string
+	AnswerFour  string
+}
+
 type Riddle struct {
-	Username string `json:"Username"`
-	Question string `json:"Question"`
-	Answer   string `json:"Answers"`
+	Username string    `json:"Username"`
+	Question string    `json:"Question"`
+	Answer   []Answers `json:"Answers"`
 }
