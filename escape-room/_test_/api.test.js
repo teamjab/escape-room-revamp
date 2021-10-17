a826b4557908252cd8c4487088904f18d5927c4e
Brend <brendonhampton1988@gmail.com>
2021-10-17 02:05:33 -0700

    added mock api test server using supertest and express

--- escape-room/_test_/api.test.js
+++
@@ -1,9 +0,0 @@
-const sum = require("./apitest.js");
-
-const url = "https://catfact.ninja/breeds";
-// basic starter test to link up test suite
-describe("api tests", () => {
-  test("adds 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3)
-  })
-});
