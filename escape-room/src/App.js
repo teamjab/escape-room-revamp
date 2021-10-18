ceb105b2c30ac154de3515efe0413c407eb1594d
Brend <brendonhampton1988@gmail.com>
2021-10-18 16:38:09 -0700

    added router, created pages directory with landing,game,score pages

--- escape-room/src/App.js
+++ escape-room/src/App.js
@@ -1,7 +1,9 @@
 import userForm from "./components/userForm.js";
 import {useSelector} from 'react-redux';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './styling/App.scss';
-
+import Landing from './pages/landing/landing.js';
+import Game from './pages/game/game.js';
 
 
 
@@ -11,31 +13,22 @@ function App() {
   console.log(name.player);
   
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <main>
-        <h1>
-        <span>E</span>
-        <span>s</span>
-        <span>c</span>
-        <span>a</span>
-        <span>p</span>
-        <span>e</span>
-        <span>R</span>
-        <span>o</span>
-        <span>o</span>
-        <span>m!</span>
-        </h1>
-        <form>
-          <label>Username: </label>
-          <input placeholder="enter username"></input>
-        </form>
-        <button type="submit">Submit</button>
-      </main>
-      <footer>
-        <h4>Team Jab</h4>
-      </footer>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header"></header>
+        <main>
+          <Landing></Landing>
+          <form>
+              <label>Username: </label>
+              <input placeholder="enter username"></input>
+          </form>
+          <button type="submit">Submit</button>
+        </main>
+        <footer>
+          <h4>Team Jab</h4>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
---
+++ escape-room/src/pages/landing/landing.js
@@ -0,0 +1,21 @@
+
+const Landing = () => {
+    return (
+      <main>
+          <h1>
+          <span>E</span>
+          <span>s</span>
+          <span>c</span>
+          <span>a</span>
+          <span>p</span>
+          <span>e</span>
+          <span>R</span>
+          <span>o</span>
+          <span>o</span>
+          <span>m!</span>
+          </h1>
+        </main>
+    )
+  };
+  
+  export default Landing;
