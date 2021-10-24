9f4b91828751b3006c87b0c311ed2722c33d4a6b
Brend <brendonhampton1988@gmail.com>
2021-10-23 22:22:16 -0700

    pushing up code that current bug

--- escape-room/src/pages/game/game.js
+++ escape-room/src/pages/game/game.js
@@ -5,12 +5,18 @@ function Game() {
   const player = store.getState();
   const playerName = player.player.username;
   const riddle = store.getState();
+  const riddles = riddle.riddles;
+  
+  const getState = () => {
+    console.log(store.getState());
+  }
 
 
     return (
       <main className='game-page'>
         <h1>welcome {playerName}</h1>
         <h3>here is your riddle???</h3>
+        <button onClick={getState}>test</button>
         <p></p>
       </main>
       
