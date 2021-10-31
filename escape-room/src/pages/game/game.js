1cc6c5e6385e4496d7c64c0003f2a814424d0554
Brend <brendonhampton1988@gmail.com>
2021-10-31 01:15:09 -0700

    adding doors/pages to the game for each riddle

--- escape-room/src/pages/game/game.js
+++ escape-room/src/pages/game/game.js
@@ -1,23 +1,27 @@
 import store from '../../store/store';
 import '../../styling/game/game.scss';
-function Game() {
-    
-  const player = store.getState();
-  const playerName = player.player.username;
-  const riddle = store.getState();
-  const riddles = riddle.riddles;
+
+function Game() {   
+const player = store.getState();
+const playerName = player.player.username;
   
-  const getState = () => {
+  const getRiddles = () => {
+    player.player.score += 100;
     console.log(store.getState());
+
   }
 
 
     return (
       <main className='game-page'>
         <h1>welcome {playerName}</h1>
-        <h3>here is your riddle???</h3>
-        <button onClick={getState}>test</button>
-        <p></p>
+        <p>Upon entering the House you notice a dim lit hallway teaming with doors all around you. You take note that the room inside looks nothing comparable to the house as it looked outside.</p>
+        <p>Even stranger, you notice numbers on the doors, almost beckoning you to enter through one.</p>
+        <button onClick={getRiddles}>First Door</button>
+        <button onClick={getRiddles}>Second Door</button>
+        <button onClick={getRiddles}>Third Door</button>
+        <button onClick={getRiddles}>Fourth Door</button>
+        
       </main>
       
     );
